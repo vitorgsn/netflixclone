52c259c277062c4a9a16245b511f8153382f4957
Vitor Nascimento <dev.vitor99@gmail.com>
2021-11-22 22:33:00 -0300

    pimeiro commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default () => {
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+    const [balckHeader, setBalckHeader] = useState(false);
+
+    //executado ao carregar a tela
+    useEffect(() => {
+        const loadAll = async () => {
+            //Pegando a lista TOTAL
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+            // Pegando o Featured
+            let originals = list.filter(i => i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeaturedData(chosenInfo);
+
+        }
+
+        loadAll();
+    }, []);
+
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 10) {
+                setBalckHeader(true);
+            } else {
+                setBalckHeader(false);
+            }
+        }
+
+        window.addEventListener('scroll', scrollListener);
+
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+    }, []);
+
+    return (
+        <div className="page">
+
+            <Header black={balckHeader} />
+
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
+
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
+
+            <footer>
+                Feito com <span role="img" aria-label="coração">❤️</span> por Vitor Gabriel<br />
+                Direitos de imagem para Netflix<br />
+                Dados pegos do site Themoviedb.org
+            </footer>
+        </div>
+    );
+}
+
+//https://youtu.be/tBweoUiMsDg?t=9698
---
+++ src/components/MovieRow.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+export default ({ title, items }) => {
+
+    const handleLeftArrow = () => {
+
+    }
+
+    const handleRightArrow = () => {
+
+    }
+
+    return (
+        <div className="movieRow">
+            <h2>
+                {title}
+            </h2>
+
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{ fontSize: 50 }} />
+
+            </div>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <NavigateNextIcon style={{ fontSize: 50 }} />
+
+            </div>
+
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
